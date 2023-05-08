9c28245b382953a8e0e474815c999eb53b2be9ce
brayandev90 <bpa787448@gmail.com>
2023-05-07 22:16:15 -0700

    update next UI

--- apps/dev/nextjs/pages/index.js
+++ apps/dev/nextjs/pages/index.js
@@ -3,9 +3,9 @@ import Layout from "components/layout"
 export default function Page() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>NextAuth.js</h1>
       <p>
-        This is an example site to demonstrate how to use{" "}
+        This is an sample site to demonstrate how to use{" "}
         <a href="https://authjs.dev">NextAuth.js</a> for authentication.
       </p>
     </Layout>
